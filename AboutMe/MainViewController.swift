d6995ab13e45c3bb31b6db9220a0ff1a04e857b4
jjbuell-dev <84111581+jjbuell-dev@users.noreply.github.com>
2022-06-16 16:23:19 -0600

    initial Commit, no Storyboard

---
+++ AboutMe/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+// MainViewController.swift
+// AboutMe
+//
+
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+
+
+}
+
